e23316df20d9480ead086701daba9877dc60d417
Paul Kazarin <paul.kazarin@wgu.edu>
2023-04-11 11:14:52 -0600

    images

--- src/main/java/com/example/c868Backend/Type.java
+++ src/main/java/com/example/c868Backend/Type.java
@@ -11,10 +11,14 @@ public class Type {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String type;
+    private String image;
 
     public Integer getId() {return id;}
     public void setId(Integer id) {this.id = id;}
 
     public String getType() {return type;}
     public void setType(String type) {this.type = type;}
+
+    public String getImage() {return image;}
+    public void setImage(String image) {this.image = image;}
 }
